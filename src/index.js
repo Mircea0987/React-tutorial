d111d8baeda2e6e1032138161c1669557e2fdd5e
=Calugaru Mircea-Costin <=>
2022-10-29 15:32:17 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import {createRoot} from 'react-dom/client'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function BookList(){
+    return (
+        <section>
+            This is a bookList
+            <article className='bookList'>
+                <Book/>
+                <Book/>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+            </article>
+        </section>
+    )
+}
+const Book = () =>{
+    return(
+        <div className='book'>
+            <Image/>
+            <Title/>
+            <Author/>
+            <Price/>
+        </div>
+    )
+}
+const Image = () =>{
+    return(
+        <div>
+            <img src='https://m.media-amazon.com/images/I/41mo3-FGjXL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg'></img>
+        </div>
+    )
+}
+const title = 'Harry Potter'
+const Title = () =>{
+    return(
+        <div>
+            <h1>{title.toUpperCase()}</h1>
+        </div>
+    )
+}
+const Author = () =>{
+    const author = 'J. K. Rowling'
+
+    return(
+        <div>
+            <h1 style={{color:'red',fontSize:'0.75rem'}}>{author}</h1>
+        </div>
+    )
+}
+const Price = () => <h1>38.98$</h1>
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<BookList/>)
