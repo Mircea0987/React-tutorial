ed615aecf117d37e4905c3de46aaf1c722148b4c
=Calugaru Mircea-Costin <=>
2022-10-29 21:19:37 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,54 +1,62 @@
-import React from 'react'
-import {createRoot} from 'react-dom/client'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+// variabile
+const books = [
+  {
+    id: 1,
+    title: "Harry Potter",
+    author: "J. K. Rowling",
+    bookImage:
+      "https://m.media-amazon.com/images/I/41mo3-FGjXL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+    price: 38.98,
+  },
+  {
+    id: 2,
+    title: "Billy Summer",
+    author: "Stephen King",
+    bookImage:
+      "https://nemira.ro/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/s/t/stephen-king---billy-summers---coperta-fata---c1---color.jpg",
+    price: 15,
+  },
+];
 
-function BookList(){
-    return (
-        <section>
-            This is a bookList
-            <article className='bookList'>
-                <Book/>
-                <Book/>
+function BookList() {
+  return (
+    <section className="bookList">
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
+    </section>
+  );
+}
+const Book = ({ title, author, bookImage, price }) => {
+  const clickHandler = () => {
+    alert("yo");
+  };
+  const getPrice = (price) => {
+    console.log(price);
+  };
 
-            </article>
-        </section>
-    )
-}
-const Book = () =>{
-    return(
-        <div className='book'>
-            <Image/>
-            <Title/>
-            <Author/>
-            <Price/>
-        </div>
-    )
-}
-const Image = () =>{
-    return(
-        <div>
-            <img src='https://m.media-amazon.com/images/I/41mo3-FGjXL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg'></img>
-        </div>
-    )
-}
-const title = 'Harry Potter'
-const Title = () =>{
-    return(
-        <div>
-            <h1>{title.toUpperCase()}</h1>
-        </div>
-    )
-}
-const Author = () =>{
-    const author = 'J. K. Rowling'
+  return (
+    <div
+      className="book"
+      onMouseOver={() => {
+        console.log(author);
+      }}
+    >
+      <img src={bookImage} alt=""></img>
+      <h1>{title}</h1>
+      <h1>{author}</h1>
+      <h1>{price}$</h1>
+      <button type="button" onClick={clickHandler}>
+        Example
+      </button>
+      <button onClick={() => getPrice(price)}>Get Title</button>
+    </div>
+  );
+};
 
-    return(
-        <div>
-            <h1 style={{color:'red',fontSize:'0.75rem'}}>{author}</h1>
-        </div>
-    )
-}
-const Price = () => <h1>38.98$</h1>
-const container = document.getElementById('root')
-const root = createRoot(container)
-root.render(<BookList/>)
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<BookList />);
